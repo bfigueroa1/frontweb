95dcfd81d7ac37d1d3427b2071eb27bbd3752ed7
bfigueroa1@uc.cl <you@example.com>
2020-10-14 17:44:56 -0300

    attack

--- src/assets/js/map.js
+++ src/assets/js/map.js
@@ -7,18 +7,169 @@ function range(end) {
     return ans;
 }
 
+// ATTACK elementos de localStorage del enemigo cuando es atacado
+
+var boton_t = document.getElementById("attack-troopsBtn");
+
+boton_t.addEventListener("click", function(){
+
+	t_ls1 = localStorage.getItem('tropas_1');
+	t_ls2 = localStorage.getItem('tropas_2');
+	at_ls = localStorage.getItem('number_t');
+	m_ls2 = localStorage.getItem('muralla_2');
+
+	if (parseInt(t_ls1) > 0) {
+		if (parseInt(t_ls1) > parseInt(at_ls)) {
+			if (parseInt(m_ls2) > 0) {
+				if (parseInt(m_ls2) > parseInt(at_ls)) {
+					x = parseInt(m_ls2) - parseInt(at_ls);
+					localStorage.setItem("muralla_2",x);
+
+					t = parseInt(t_ls1) - parseInt(at_ls);
+					localStorage.setItem("tropas_1",t);
+				};
+				if (parseInt(m_ls2) < parseInt(at_ls)) {
+					x = 0;
+					localStorage.setItem("muralla_2",x);
+
+					x =   parseInt(t_ls2) + parseInt(m_ls2) - parseInt(at_ls);
+					localStorage.setItem("tropas_2",x);
+
+				};
+				if (parseInt(m_ls2) == parseInt(at_ls)) {
+					x=0
+					localStorage.setItem("muralla_2",x);
+
+					t = parseInt(t_ls1) - parseInt(at_ls);
+					localStorage.setItem("tropas_1",t);
+				};
+			} else {
+				if (parseInt(m_ls2) > parseInt(at_ls)) {
+					x = parseInt(m_ls2) - parseInt(at_ls);
+					localStorage.setItem("muralla_2",x);
+
+					t = parseInt(t_ls1) - parseInt(at_ls);
+					localStorage.setItem("tropas_1",t);
+				};
+				if (parseInt(m_ls2) < parseInt(at_ls)) {
+					x = 0;
+					localStorage.setItem("muralla_2",x);
+
+					x =   parseInt(t_ls2) + parseInt(m_ls2) - parseInt(at_ls);
+					localStorage.setItem("tropas_2",x);
+
+				};
+				if (parseInt(m_ls2) == parseInt(at_ls)) {
+					x=0
+					localStorage.setItem("muralla_2",x);
+
+					t = parseInt(t_ls1) - parseInt(at_ls);
+					localStorage.setItem("tropas_1",t);
+				};
+			};
+		};
+	
+    
+});
+
+var boton_at_av = document.getElementById("attack-airplaneBtn");
+
+boton_at_av.addEventListener("click", function(){
+	av_ls1 = localStorage.getItem('aviones_2');
+	
+	if (parseInt(av_ls1) > 0) {
+		x = parseInt(av_ls1,10) - 1;
+    	localStorage.setItem("aviones_2",x);
+	} else {
+		t_ls2 = localStorage.getItem('tropas_2');
+		if (parseInt(t_ls2) > 0) {
+			x = parseInt(t_ls2,10) - 5;
+			if (x < 0) {
+				x = 0
+				localStorage.setItem("tropas_2",x);
+			} else {
+				localStorage.setItem("tropas_2",x);
+			};
+			
+		};
+	};
+    
+});
+
+var boton_at_b = document.getElementById("attack-shipBtn");
+
+boton_at_b.addEventListener("click", function(){
+	b_ls1 = localStorage.getItem('buques_2');
+	if (parseInt(b_ls1) > 0) {
+		x = parseInt(b_ls1,10) - 1;
+    	localStorage.setItem("buques_2",x);
+	} else {
+		t_ls2 = localStorage.getItem('tropas_2');
+			if (parseInt(t_ls2) > 0) {
+				x = parseInt(t_ls2,10) - 5;
+				if (x < 0) {
+					x = 0
+					localStorage.setItem("tropas_2",x);
+				} else {
+					localStorage.setItem("tropas_2",x);
+				};
+			};
+	};
+    
+});
+
+//RECOLECT 
+
+var boton_r = document.getElementById("search-resourcesBtn");
+
+boton_r.addEventListener("click", function(){
+
+    m_ls1 = localStorage.getItem('metal_1');
+    x = parseInt(m_ls1,10) + 1;
+	localStorage.setItem("metal_1",x);
+	
+	c_ls1 = localStorage.getItem('cemento_1');
+    x = parseInt(c_ls1,10) + 1;
+	localStorage.setItem("cemento_1",x);
+	
+	al_ls1 = localStorage.getItem('alimento_1');
+    x = parseInt(al_ls1,10) + 1;
+    localStorage.setItem("alimento_1",x);
+});
+
+
+
+
 // Jquery functionality
 $(function () {
 	// open modal when a territory is clicked
 	$('.modal-openBtn').click(function () {
 		$('.modal').addClass('modal-open');
+		// close troops attack menu if is open
+		$('#troops-attackMenu').removeClass('modal-open');
+		// close troops attack menu if is open
+		$('#airplanes-attackMenu').removeClass('modal-open');
+		// close troops attack menu if is open
+        $('#ships-attackMenu').removeClass('modal-open');
 		// make modal draggable
 		$('.modal').draggable({handle: ".modal-header"});
+		// make modal draggable
+		$('#troops-attackMenu').draggable({handle: ".modal-header"});
+		// make modal draggable
+		$('#airplanes-attackMenu').draggable({handle: ".modal-header"});
+		// make modal draggable
+		$('#ships-attackMenu').draggable({handle: ".modal-header"});
   	});
 
   	//close modal
   	$('.modal-closeBtn').click(function () {
-    	$('.modal').removeClass('modal-open');
+		$('.modal').removeClass('modal-open');
+		// close troops attack menu if is open
+		$('#troops-attackMenu').removeClass('modal-open');
+		// close troops attack menu if is open
+		$('#airplanes-attackMenu').removeClass('modal-open');
+		// close troops attack menu if is open
+        $('#ships-attackMenu').removeClass('modal-open');
   	});
 
   	//Change Modal data
@@ -34,7 +185,7 @@ $(function () {
 		t_ls1 = localStorage.getItem('tropas_1');
 
 		for (let i of range(parseInt(t_ls1))){         //Agregar en range(_) la cantidad de tropas del territorio  
-			$('#troops-sent').append(`${`<option value="${i}">${i}</option>`}`)
+			$('#troops-sent').append(`${`<option value="${i}">${i}</option>`}`);
 		}
 		for (let i of range(parseInt(av_ls1))){         //Agregar en range(_) la cantidad de aviones del territorio  
 			$('#airplanes-sent').append(`${`<option value="${i}">${i}</option>`}`)
@@ -42,6 +193,7 @@ $(function () {
 		for (let i of range(parseInt(bq_ls1))){         //Agregar en range(_) la cantidad de barcos del territorio  
 			$('#ships-sent').append(`${`<option value="${i}">${i}</option>`}`)
 		}
+		
   	});
 
 	$('#medio-orienteClick').click(function () {   //Para agregar otro territorio copiar toda la función y cambiar
@@ -64,19 +216,72 @@ $(function () {
 		for (let i of range(parseInt(bq_ls2))){         //Agregar en range(_) la cantidad de barcos del territorio  
 			$('#ships-sent').append(`${`<option value="${i}">${i}</option>`}`)
 		}
+		
   	});
 	
   	//Attack botton clicked
   	$('#attack-troopsBtn').click(function () {
+    	// close troops attack menu if is open
+        $('#troops-attackMenu').removeClass('modal-open');
+
+	});
+	  
+	//Attack botton clicked
+	$('#open-troops-attackMenu').click(function () {
     	$('.modal').removeClass('modal-open');
+    	$('#troops-attackSelection').children().remove().end();
+		$('#troops-attackMenu').addClass('modal-open');
+		let numb = $("#troops-sent").val();
+		localStorage.setItem('number_t',numb);
     	if ($(".modal-title").text() == "Egipt"){
-    		$(".argelia-nigeriaPath").css("fill", "black");
-    		$(".sudanPath").css("fill", "black");
-    		$(".medio-orientePath").css("fill", "black");
-    		$(".espana-francia-italiaPath").css("fill", "black");
-    		$(".polonia-yugoeslaviaPath").css("fill", "black");
+    		$('#troops-attackSelection').append(`${`<option value=".argelia-nigeria">Argelia-Nigeria</option>`}`)
+    		$('#troops-attackSelection').append(`${`<option value=".sudan">Sudan</option>`}`)
+    		$('#troops-attackSelection').append(`${`<option value=".medio-oriente">Medio Oriente</option>`}`)
+    		$('#troops-attackSelection').append(`${`<option value=".espana-francia-italia">España-Francia-Italia</option>`}`)
+    		$('#troops-attackSelection').append(`${`<option value=".polonia-yugoeslavia">Polonia-Yugoeslavia</option>`}`)
+		} else if ($(".modal-title").text() == "Medio Oriente"){
+    		$('#troops-attackSelection').append(`${`<option value=".egipto">Egipto</option>`}`)
+    		$('#troops-attackSelection').append(`${`<option value=".moscu">Moscu</option>`}`)
+    		$('#troops-attackSelection').append(`${`<option value=".aral">Aral</option>`}`)
+    		$('#troops-attackSelection').append(`${`<option value=".india">India</option>`}`)
+    		$('#troops-attackSelection').append(`${`<option value=".polonia-yugoeslavia">Polonia-Yugoeslavia</option>`}`)
+		};
+		
+	});
+
+	//Attack botton clicked Airplanes
+	$('#attack-airplaneBtn').click(function () {
+    	$('.modal').removeClass('modal-open');
+    	$('#airplanes-attackSelection').children().remove().end();
+    	$('#airplanes-attackMenu').addClass('modal-open');
+    	for (let territory of ["vladivostok", "japon", "vietnam", "siberia", "dudinka", "medio oriente", "omsk",
+    	 "aral", "china", "india", "tchita", "mongolia", "alaska", "mackenzie", "vancouver", "groenlandia", 
+    	 "nueva york", "california", "ottawa", "labrador", "mexico", "chile-bolivia-peru-ecuador", 
+    	 "argentina-paraguay-uruguay", "brasil", "colombia-venezuela", "nueva guinea", "borneo", "sumatra", 
+    	 "australia", "inglaterra", "moscu", "suecia", "islandia", "alemania", "espana-francia-italia",
+    	 "polonia-yugoeslavia", "sudan", "el congo", "africa del sur", "argelia-nigeria", "madagascar",
+    	 "egipto"]){
+    		$('#airplanes-attackSelection').append(`${`<option value="${territory}">${territory}</option>`}`)
     	}
   	});
+
+  	//Attack botton clicked Ships
+  	$('#attack-shipBtn').click(function () {
+    	$('.modal').removeClass('modal-open');
+    	$('#ships-attackSelection').children().remove().end();
+    	$('#ships-attackMenu').addClass('modal-open');
+    	for (let territory of ["vladivostok", "japon", "vietnam", "siberia", "dudinka", "medio oriente", "omsk",
+    	 "aral", "china", "india", "tchita", "mongolia", "alaska", "mackenzie", "vancouver", "groenlandia", 
+    	 "nueva york", "california", "ottawa", "labrador", "mexico", "chile-bolivia-peru-ecuador", 
+    	 "argentina-paraguay-uruguay", "brasil", "colombia-venezuela", "nueva guinea", "borneo", "sumatra", 
+    	 "australia", "inglaterra", "moscu", "suecia", "islandia", "alemania", "espana-francia-italia",
+    	 "polonia-yugoeslavia", "sudan", "el congo", "africa del sur", "argelia-nigeria", "madagascar",
+    	 "egipto"]){
+    		$('#ships-attackSelection').append(`${`<option value="${territory}">${territory}</option>`}`)
+    	}
+  	});
+	  
+  
   
   	//Territories box
 
@@ -1012,131 +1217,5 @@ $(function () {
   	});
 });
 
-
-//FUNCION para el SELECT con localStorage
-/*
-function my_Function(){
-
-    av_ls1 = localStorage.getItem('aviones_1');
-    bq_ls1 = localStorage.getItem('buques_1');
-    t_ls1 = localStorage.getItem('tropas_1');
-    /*
-    var av_1 = document.getElementById("aviones_1");
-    av_1.innerHTML = 0;
-    var bq_1 = document.getElementById("buques_1");
-    bq_1.innerHTML = 0;
-    var bq_1 = document.getElementById("buques_1");
-    bq_1.innerHTML = 0;
-   
-    var select = document.getElementById('troops'); 
-
-    for (var i = 0; i<= t_ls1; i++){
-
-        var option = document.createElement('option');
-        option.value = i;
-        option.innerHTML = i;
-        select.options.add(option);
-    }
-    
-    var select = document.getElementById('airplanes'); 
-    
-    for (var i = 0; i<= av_ls1; i++){
-    
-        var option = document.createElement('option');
-        option.value = i;
-        option.innerHTML = i;
-        select.options.add(option);
-    }
-    
-    var select = document.getElementById('ship'); 
-    
-    for (var i = 0; i<= bq_ls1; i++){
-    
-        var option = document.createElement('option');
-        option.value = i;
-        option.innerHTML = i;
-        select.options.add(option);
-    }
-
-}
- */
-// ATTACk elementos de localStorage del enemigo cuando es atacado
-
-var boton_t = document.getElementById("attack-troopsBtn");
-
-boton_t.addEventListener("click", function(){
-	t_ls1 = localStorage.getItem('tropas_2');
-	
-	if (parseInt(t_ls1) > 0) {
-		x = parseInt(t_ls1,10) - 1;
-    	localStorage.setItem("tropas_2",x);
-	};
-    
-});
-
-var boton_at_av = document.getElementById("attack-airplaneBtn");
-
-boton_at_av.addEventListener("click", function(){
-	av_ls1 = localStorage.getItem('aviones_2');
-	
-	if (parseInt(av_ls1) > 0) {
-		x = parseInt(av_ls1,10) - 1;
-    	localStorage.setItem("aviones_2",x);
-	} else {
-		t_ls1 = localStorage.getItem('tropas_2');
-		if (parseInt(t_ls1) > 0) {
-			x = parseInt(t_ls1,10) - 5;
-			if (x < 0) {
-				x = 0
-				localStorage.setItem("tropas_2",x);
-			} else {
-				localStorage.setItem("tropas_2",x);
-			};
-			
-		};
-	};
-    
-});
-
-var boton_at_b = document.getElementById("attack-shipBtn");
-
-boton_at_b.addEventListener("click", function(){
-	b_ls1 = localStorage.getItem('buques_2');
-	if (parseInt(b_ls1) > 0) {
-		x = parseInt(b_ls1,10) - 1;
-    	localStorage.setItem("buques_2",x);
-	} else {
-		t_ls1 = localStorage.getItem('tropas_2');
-			if (parseInt(t_ls1) > 0) {
-				x = parseInt(t_ls1,10) - 5;
-				if (x < 0) {
-					x = 0
-					localStorage.setItem("tropas_2",x);
-				} else {
-					localStorage.setItem("tropas_2",x);
-				};
-			};
-	};
-    
-});
-
-//RECOLECT 
-
-var boton_r = document.getElementById("search-resourcesBtn");
-
-boton_r.addEventListener("click", function(){
-
-    m_ls1 = localStorage.getItem('metal_1');
-    x = parseInt(m_ls1,10) + 1;
-	localStorage.setItem("metal_1",x);
-	
-	c_ls1 = localStorage.getItem('cemento_1');
-    x = parseInt(c_ls1,10) + 1;
-	localStorage.setItem("cemento_1",x);
-	
-	al_ls1 = localStorage.getItem('alimento_1');
-    x = parseInt(al_ls1,10) + 1;
-    localStorage.setItem("alimento_1",x);
-});
 
 
--- src/assets/js/shop.js
+++ src/assets/js/shop.js
@@ -3,16 +3,16 @@
 localStorage.setItem("aviones_1",0);
 localStorage.setItem("buques_1",0);
 localStorage.setItem("muralla_1",0);
-localStorage.setItem("tropas_1",5);
+localStorage.setItem("tropas_1",10);
 
-localStorage.setItem("metal_1",30)
+localStorage.setItem("metal_1",10)
 localStorage.setItem("cemento_1",10)
 localStorage.setItem("alimento_1",10)
 
-localStorage.setItem("aviones_2",20);
-localStorage.setItem("buques_2",20);
-localStorage.setItem("muralla_2",1);
-localStorage.setItem("tropas_2",20);
+localStorage.setItem("aviones_2",5);
+localStorage.setItem("buques_2",5);
+localStorage.setItem("muralla_2",2);
+localStorage.setItem("tropas_2",5);
 
 localStorage.setItem("metal_2",50)
 localStorage.setItem("cemento_2",50)
@@ -23,12 +23,21 @@ var boton_av = document.getElementById("btn_av");
 
 boton_av.addEventListener("click", function(){
     av_ls1 = localStorage.getItem('aviones_1');
-    x = parseInt(av_ls1,10) + 1;
-    localStorage.setItem("aviones_1",x);
-
     met = localStorage.getItem('metal_1');
-    x = parseInt(met,10) - 3;
-    localStorage.setItem("metal_1",x);
+
+    if (parseInt(met) > 0) {
+
+        a = parseInt(av_ls1,10) + 1;
+        localStorage.setItem("aviones_1",a);
+        x = parseInt(met,10) - 3;
+        if (x < 0){
+            x = 0
+            localStorage.setItem("metal_1",x);
+        } else {
+            localStorage.setItem("metal_1",x);
+        };
+    };
+    
 
 });
 
@@ -36,27 +45,46 @@ var boton_bq = document.getElementById("btn_bq");
 
 boton_bq.addEventListener("click", function(){
     bq_ls1 = localStorage.getItem('buques_1');
-    x = parseInt(bq_ls1,10) + 1;
-    localStorage.setItem("buques_1",x);
-
     met = localStorage.getItem('metal_1');
-    x = parseInt(met,10) - 3;
-    localStorage.setItem("metal_1",x);
+
+    if (parseInt(met) > 0) {
+
+        b = parseInt(bq_ls1,10) + 1;
+        localStorage.setItem("buques_1",b);
+        x = parseInt(met,10) - 3;
+        if (x < 0){
+            x = 0
+            localStorage.setItem("metal_1",x);
+        } else {
+            localStorage.setItem("metal_1",x);
+        };
+    };
+    
+    
 });
 
 var boton_m = document.getElementById("btn_m");
 
 boton_m.addEventListener("click", function(){
     m_ls1 = localStorage.getItem('muralla_1');
+    cem = localStorage.getItem('cemento_1');
 
-    if (m_ls1 == 0) {
-        x = parseInt(m_ls1,10) + 1;
-        localStorage.setItem("muralla_1",x);
+    if (parseInt(cem) > 0) {
+        m = parseInt(m_ls1,10) + 1;
+        localStorage.setItem("muralla_1",m);
 
-        cem = localStorage.getItem('cemento_1');
         x = parseInt(cem,10) - 5;
-        localStorage.setItem("cemento_1",x);
-    }
+        if (x < 0){
+            x = 0
+            localStorage.setItem("cemento_1",x);
+        } else {
+            localStorage.setItem("cemento_1",x);
+        };
+        
+    };
+    
+        
+    
     
 
 });

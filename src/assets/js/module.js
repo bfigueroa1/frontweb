3a9fd06b60d8e86d3d7720e0be57245f1a8392c9
Juan Pablo Ramirez <jpramirez5@uc.cl>
2020-11-01 17:21:36 -0300

    Reusing Navbar

---
+++ src/assets/js/module.js
@@ -0,0 +1,63 @@
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    /* Loop through a collection of all HTML elements: */
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("loadhtml");
+        if (file) {
+            Object.keys(moduleList).map(
+                (key) => {
+                    if(key===file)
+                    elmnt.innerHTML = moduleList[key];
+                }
+            )
+        }
+    }
+}
+
+const moduleList = {};
+
+moduleList.nav = `
+    <a href="../../index.html" class="link_logo">
+        <img class= "logo" src="../assets/imgs/Logo Blanco.png" alt="logo">
+    </a>
+    <ul class="navbar_items">
+        <li class="navbar_item">
+            <a href="rules.html" class="navbar_link">Rules</a>
+        </li>
+        <li class="navbar_item">
+            <a href="about.html" class="navbar_link">About Us</a>
+        </li>
+        <li class="navbar_item">
+            <a href="contact.html" class="navbar_link">Contact Us</a>
+        </li>
+    </ul>
+    <a href="log_in.html"><button class="log_in">Login</button></a>
+    <a href="sign_up.html"><button class="sign_up">Sign Up</button></a>
+`
+moduleList.navloged = `
+    <nav class="navbar"  id="navloged">
+        <a href="index loged.html" class="link_logo">
+            <img class= "logo" src="../assets/imgs/Logo Blanco.png" alt="logo">
+        </a>    
+        <ul class="navbar_items">
+            <li class="navbar_item">
+                <a href="player.html" class="navbar_link">Player</a>
+            </li>
+            <li class="navbar_item">
+                <a href="army.html" class="navbar_link">Army</a>
+            </li>
+            <li class="navbar_item">
+                <a href="territories.html" class="navbar_link">Territories</a>
+            </li>
+            <li class="navbar_item">
+                <a href="start_playing.html" class="navbar_link">Game Session</a>
+            </li>
+        </ul>
+        <a href="../../index.html"><button class="log_out">Logout</button></a>
+    </nav>
+`
+
+window.onload = includeHTML();
